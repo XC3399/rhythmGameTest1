23f059fcecd17180e75a87ba11abf0757910a903
XC3399 <113364159+XC3399@users.noreply.github.com>
2024-02-14 16:24:10 +0000

    before changing callback functions

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_BGMUSICPLAYLIST = 54190626U;
+        static const AkUniqueID PLAY_ENEMYPREATTACK = 2918121273U;
     } // namespace EVENTS
 
     namespace BANKS
--- Assets/audio/musicManager.cs
+++ Assets/audio/musicManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class musicManager : MonoBehaviour
 {
     [SerializeField] GameObject listener;
-    [SerializeField] AK.Wwise.Event mainMusic;
 
+    [SerializeField] AK.Wwise.Event mainMusic;
     public int cueCount = 0;
     public bool window = true;
+
+    [SerializeField] AK.Wwise.Event enemyPreAttack;
+    public bool enemyInRange = false;
+    public int timesInRange = 0;
+    public int debugNum = 0;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         mainMusic.Post(listener, (uint)AkCallbackType.AK_MusicSyncUserCue, setWindow);
-        
+        //enemyPreAttack.Post(listener, (uint)AkCallbackType.AK_MusicSyncBeat, preAttack);
     }
 
     public void setWindow (object in_cookie, AkCallbackType in_type, object in_info)
@@ -28,5 +35,30 @@ public class musicManager : MonoBehaviour
         {
             window = false;
         }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            enemyInRange = true;
+        }
+        if (timesInRange == 3)
+        {
+            enemyInRange = false;
+        }
+    }
+
+    public void preAttack (object in_cookie, AkCallbackType in_type, object in_info)
+    {
+        Debug.Log(debugNum);
+        debugNum = debugNum + 1;
+
+        if (enemyInRange == true)
+        {
+            enemyPreAttack.Post(listener);
+            timesInRange = timesInRange + 1;
+        }
+
     }
 }
